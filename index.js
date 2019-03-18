b682b78f6911bdfd3693d6daae661bfa80a64c63
viymak <viymak@icloud.com>
2019-03-17 21:12:08 -0400

    added indexing code

---
+++ index.js
@@ -0,0 +1,146 @@
+// for Emmet
+
+const infile = 'test/data/ca_schools_lead_testing_data_geocoded.csv';
+const tableNames = [
+	['county', 'city'],
+	['county', 'schoolName'],
+	['city', 'schoolName']
+];
+
+const { readFileSync, writeFileSync } = require('fs');
+
+const { convertCSVToArray } = require('convert-csv-to-array');
+
+
+const ObjectsToCsv = require('objects-to-csv');
+
+
+function sortByNumber(a, b) {
+	return a - b;
+}
+
+
+const data = readFileSync(infile, 'utf-8');
+
+// remove \r
+const cleaned = data.replace(/\r/g, '');
+
+const rows = convertCSVToArray(cleaned).slice(1);
+
+const columns = Object.keys(rows[0]).sort();
+
+console.log("columns:", columns);
+
+const uniques = {};
+columns.forEach(column => {
+	uniques[column] = new Set();
+});
+
+rows.forEach(row => {
+	columns.forEach(column => {
+		uniques[column].add(row[column]);
+	});
+});
+
+// figure out what to index and what not to
+const indexTheseColumns = new Set();
+tableNames.forEach(([a, b]) => {
+	indexTheseColumns.add(a);
+	indexTheseColumns.add(b);
+});
+
+// sort unique values for each column
+for (let columnName in uniques) {
+	// e.g. San Diego, San Francisco, San Pablo
+	const values = Array.from(uniques[columnName]).sort();
+	uniques[columnName] = values;
+
+	// save to index / lookup table
+	const filename = "output/indexes/" + columnName + "-index.tsv";
+	const fileText = values.join("\n") + "\n";
+	writeFileSync(filename, fileText, "utf-8");		
+}
+console.log('uniques', uniques);
+
+// replace values with index numbers
+rows.forEach(row => {
+	for (let column in row) {
+		const originalValue = row[column];
+		row[column] = uniques[column].indexOf(originalValue);
+	}
+});
+
+//const outfile = infile.replace(".csv", "_compressed.csv");
+const outfile = 'output/compressed.csv';
+new ObjectsToCsv(rows).toDisk(outfile);
+
+
+// allow a user to specify tableNames
+/*
+const tableNames = new Set();
+columns.forEach(a => {
+	// e.g., column = city
+	columns.forEach(b => {
+		if (a !== b) {
+			// e.g. a = county, b = city
+			const tableName = JSON.stringify([a, b].sort());
+			tableNames.add(tableName);
+		}
+	})
+});
+*/
+
+console.log("tableNames:", tableNames);
+
+/*
+	e.g. {
+		'["county", "city"]': {
+			"Alameda County": {Berekley, Dublin, San Pablo},
+			"San Diego County": {San Diego City, El Cajon City}
+		},
+		'["county", "school"]': {
+			"San Diego County": "University of San Diego High School"
+		}
+	}
+*/
+const coocurrences = {};
+tableNames.forEach(tableName => {
+	const [a, b] = tableName; //e.g. [county, city]
+	console.log("a:", a);
+	console.log("b:", b);
+	const table = {};
+	rows.forEach(row => {
+		const a_value = row[a]; //e.g. Alameda County
+		const b_value = row[b]; //e.g. Berkeley
+		if (a_value in table) {
+			table[a_value].add(b_value);
+		} else {
+			table[a_value] = new Set([b_value]);
+		}
+	});
+	// sort b values
+	for (let a_value in table) {
+		table[a_value] = Array.from(table[a_value]).sort(sortByNumber);
+	}
+
+	coocurrences[JSON.stringify(tableName)] = table;
+});
+
+//county,cities
+//54	12,31,41,67,123
+for (let tableName in coocurrences) {
+	const tableNameArray = JSON.parse(tableName);
+	const filename = "output/cooccurrences/" + tableNameArray.join("-to-") + '.tsv';
+	console.log(filename);
+	const table = coocurrences[tableName];
+	let fileText = tableNameArray.join("\t") + "\n";
+	for (let aNumber in table) {
+		fileText += aNumber + "\t";
+		const bNumbers = table[aNumber];
+		console.log(bNumbers);
+		fileText += bNumbers.join(",");
+		fileText += "\n";
+	}
+	writeFileSync(filename, fileText, 'utf-8');
+}
+
